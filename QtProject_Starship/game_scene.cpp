0c98417dc155bc2e10a01edc692f5cbc3f5ea80c
leason-lyx <2200013104@stu.pku.edu.cn>
2023-06-27 10:33:42 +0800

    change background

--- QtProject_Starship/game_scene.cpp
+++ QtProject_Starship/game_scene.cpp
@@ -12,9 +12,9 @@ void game_scene::showEvent(QShowEvent* event)
 {
     QGraphicsView::showEvent(event);
 
-    QPixmap pixmap(":/images/background2.jpg");
+    QPixmap pixmap(":/images/background4.jpg");
     pixmap = pixmap.scaled(this->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
-//    pixmap = pixmap.scaled(this->size(), Qt::KeepAspectRatio);
+    pixmap = pixmap.scaled(this->size(), Qt::KeepAspectRatio);
     QBrush brush(pixmap);
     this->setBackgroundBrush(brush);
 }
@@ -23,8 +23,8 @@ void game_scene::resizeEvent(QResizeEvent* event)
 {
     QGraphicsView::resizeEvent(event);
 
-    QPixmap pixmap(":/images/background.png");
-//    pixmap = pixmap.scaled(this->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    QPixmap pixmap(":/images/background4.jpg");
+    pixmap = pixmap.scaled(this->size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
     pixmap = pixmap.scaled(this->size(),Qt::IgnoreAspectRatio);
     this->setBackgroundBrush(pixmap);
 }
