6b633a6691d301a15b056174dea512f3662dfd85
leason-lyx <2200013104@stu.pku.edu.cn>
2023-06-30 21:21:44 +0800

    delete useless code

--- QtProject_Starship/main_menu.cpp
+++ QtProject_Starship/main_menu.cpp
@@ -33,10 +33,6 @@ void main_menu::on_pushButton_clicked()
     scene->setSceneRect(-300, -300, 600, 600);
     scene->setItemIndexMethod(QGraphicsScene::NoIndex);
 
-//    ship = new Starship();
-//    ship->setPos(QPointF(0,0));
-//    scene->addItem(ship);
-
     //QGraphicsTextItem clock_prompt=new QGraphicsTextItem(" TIME: 00:00 ");
     clock_prompt* clock_object=new clock_prompt();
     clock_object->setPos(500,450);
@@ -58,31 +54,5 @@ void main_menu::on_pushButton_clicked()
     timer->start(1000 / 33);
 
     qDebug("timer complete");
-
-//    QGraphicsScene scene;
-//    scene.setSceneRect(-300, -300, 600, 600);
-//    //! [1] //! [2]
-//    scene.setItemIndexMethod(QGraphicsScene::NoIndex);
-//    //! [2]
-//    //! [3]
-//    Starship ship;
-//    ship.setPos(QPointF(0,0));
-//    scene.addItem(&ship);
-//    //! [4]
-//    game_scene view(&scene);
-//    view.setRenderHint(QPainter::Antialiasing);
-//    //! [4] //! [5]
-//    view.setCacheMode(QGraphicsView::CacheBackground);
-//    view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
-//    view.setDragMode(QGraphicsView::ScrollHandDrag);
-//    //! [5] //! [6]
-//    view.setWindowTitle(QT_TRANSLATE_NOOP(QGraphicsView, "Starship Trek"));
-//    view.resize(1000, 800);
-//    view.show();
-//    qDebug("view show complete");
-//    QTimer timer;
-//    QObject::connect(&timer, &QTimer::timeout, &scene, &QGraphicsScene::advance);
-//    timer.start(1000 / 33);
-//    qDebug("timer complete");
 }
 
