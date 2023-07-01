18f601f1dbb33c11edf486b8ca48cd5001c51af7
董博瑄 <dobx2622@aliyun.com>
2023-07-01 18:29:42 +0800

    飞船动效改善

--- QtProject_Starship/main.cpp
+++ QtProject_Starship/main.cpp
@@ -12,29 +12,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     main_menu menu;
     menu.show();
-//    QGraphicsScene scene;
-//    scene.setSceneRect(-300, -300, 600, 600);
-//    //! [1] //! [2]
-//    scene.setItemIndexMethod(QGraphicsScene::NoIndex);
-//    //! [2]
-//    //! [3]
-//    Starship ship;
-//    ship.setPos(QPointF(0,0));
-//    scene.addItem(&ship);
-//    //! [4]
-//    game_scene view(&scene);
-//    view.setRenderHint(QPainter::Antialiasing);
-//    //! [4] //! [5]
-//    view.setCacheMode(QGraphicsView::CacheBackground);
-//    view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
-//    view.setDragMode(QGraphicsView::ScrollHandDrag);
-//    //! [5] //! [6]
-//    view.setWindowTitle(QT_TRANSLATE_NOOP(QGraphicsView, "Starship Trek"));
-//    view.resize(1000, 800);
-//    view.show();
-
-//    QTimer timer;
-//    QObject::connect(&timer, &QTimer::timeout, &scene, &QGraphicsScene::advance);
-//    timer.start(1000 / 33);
     return a.exec();
 }
--- QtProject_Starship/starship.cpp
+++ QtProject_Starship/starship.cpp
@@ -33,16 +33,32 @@ QPainterPath Starship::shape() const{
 
 void Starship::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
 {
+    painter->setRenderHint(QPainter::Antialiasing, true);
     //Header
-    painter->setBrush(QColorConstants::Svg::goldenrod);
+    QLinearGradient linear(QPointF(-8, -20), QPointF(4, -20));
+    linear.setColorAt(0, QColorConstants::Svg::goldenrod);
+    linear.setColorAt(1, QColorConstants::Svg::lightgoldenrodyellow);
+    //    painter->setBrush(QColorConstants::Svg::goldenrod);
+    linear.setSpread(QGradient::PadSpread);
+    painter->setBrush(linear);
     painter->drawEllipse(-8, -20, 16, 16);
 
     //Body
-    painter->setBrush(QColorConstants::Svg::floralwhite);
+    linear = QLinearGradient(QPointF(-8, -12), QPointF(4, -12));
+    linear.setColorAt(0, QColorConstants::Svg::gray);
+    linear.setColorAt(1, QColorConstants::Svg::floralwhite);
+    linear.setSpread(QGradient::PadSpread);
+    painter->setBrush(linear);
+    //    painter->setBrush(QColorConstants::Svg::floralwhite);
     painter->drawRect(-8, -12, 16, 20);
 
     //Legs
-    painter->setBrush(QColorConstants::Svg::sienna);
+    linear = QLinearGradient(QPointF(-20, -20), QPointF(20, -20));
+    linear.setColorAt(0, QColorConstants::Svg::saddlebrown);
+    linear.setColorAt(1, QColorConstants::Svg::sienna);
+    linear.setSpread(QGradient::PadSpread);
+    painter->setBrush(linear);
+    //    painter->setBrush(QColorConstants::Svg::sienna);
     QPoint pointL[3], pointR[3];
     pointL[0].setX(-8);pointL[0].setY(0);
     pointL[1].setX(-8);pointL[1].setY(8);
@@ -54,7 +70,12 @@ void Starship::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidge
     painter->drawPolygon(pointR, 3);
 
     //Engines
-    painter->setBrush(QColorConstants::Svg::burlywood);
+    linear = QLinearGradient(QPointF(-24, -20), QPointF(24, -20));
+    linear.setColorAt(0, QColorConstants::Svg::burlywood);
+    linear.setColorAt(1, QColorConstants::Svg::wheat);
+    linear.setSpread(QGradient::PadSpread);
+    painter->setBrush(linear);
+    //    painter->setBrush(QColorConstants::Svg::burlywood);
     painter->drawRect(-24, 18, 8, 2);
     painter->drawRect(16, 18, 8, 2);
 }
@@ -65,15 +86,15 @@ void Starship::advance(int step){
         //velocity
         velocity = velocity + acceleration;
         if(velocity >= 5)velocity = 5;
-        if(velocity <= -2)velocity = -2;
-        if(acceleration == 0 && velocity > 0) velocity -= 0.1;
-        else if(acceleration == 0 && velocity < 0) velocity += 0.1;
+        if(velocity <= -3)velocity = -3;
+        if(acceleration == 0 && velocity > 0) velocity -= 0.01;
+        else if(acceleration == 0 && velocity < 0) velocity += 0.01;
 
         //angular velocity
         angular_v = angular_v + angular_a;
         angle = angle + angular_v;
-        if(angular_v >= 2)angular_v = 2;
-        if(angular_v <= -2)angular_v = -2;
+        if(angular_v >= 3)angular_v = 3;
+        if(angular_v <= -3)angular_v = -3;
         if(angular_a == 0 && angular_v > 0) angular_v -= 0.1;
         else if(angular_a == 0 && angular_v < 0) angular_v += 0.1;
 
